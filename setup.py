da7a498ffe498459bc6196827505ff2806fe33dd
Rituparna Sharma <rituparna.sharma@thedatateam.com>
2022-06-12 13:00:42 +0530

    web module added

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
-        name = "src" , 
-        version = "0.0.1",
-        description = "Its a Mlops package",
-        author = "Rituparna",
-        packages = find_packages(),
-        licences = "MIT"
+    name="src",
+    version="0.0.1",
+    description="Its a Mlops package",
+    author="Rituparna",
+    packages=find_packages(),
+    licences="MIT"
 
 )
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,11 @@
+import pytest
+
+
+class NotInRange(Exception):
+    def __init__(self, message="value not in range"):
+        self.message = message
+        super().__init__(self.message)
+
 
 def test_generic():
-    assert 5==2
+    assert 5 == 2
