7f300637372ab5324b0cd4f2a103445b3c87522d
Rituparna Sharma <rituparna.sharma@thedatateam.com>
2022-06-12 11:53:25 +0530

    updated with setup file and tests file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+        name = "src" , 
+        version = "0.0.1",
+        description = "Its a Mlops package",
+        author = "Rituparna",
+        packages = find_packages(),
+        licences = "MIT"
+
+)
